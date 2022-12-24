b01cba84531bd330f30284cdefe6adf2c0ef21bc
Henrique Moreira <h@serrasqueiro.com>
2022-12-24 13:11:08 +0000

    Latin-1 encoding!

--- src/dump_sagas.py
+++ src/dump_sagas.py
@@ -6,8 +6,10 @@
 # pylint: disable=missing-function-docstring
 
 import sys
-from jdba.jbox import JBox
 import jdba
+
+IO_ENCODING = "ISO-8859-1"
+
 
 def main():
     if runner(sys.argv[1:]) is None:
@@ -15,25 +17,38 @@ def main():
 {__file__} command
 
 Commands are:
+   a           ASCII based dump
    d           Dump 'sagas'
+   s           Save database
 """)
 
 def runner(args, debug=0):
     param = args if args else ["d"]
     what = param[0]
     del param[0]
+    if what not in "ads":
+        return None
     opts = {}
     return do_this(what, param, opts, debug)
 
 def do_this(what, param, opts, debug=0):
-    is_ok, infos, dbx = get_db(debug=1)
+    assert not opts
+    assert not param
+    is_ok, infos, dbx = get_db()
     assert is_ok, dbx.name
-    dbx.set_db_save("d")  # Write database if different
+    #dbx.set_db_save("d")  # Write database if different
     if not is_ok:
         print("Invalid paths:", dbx.path_refs())
         return None
-    assert not param
-    res = my_script(what, dbx, infos)
+    if what == "s":
+        res = dbx.save()
+        print("dbx.save():", res)
+        return dbx.table_names()
+    res = my_script(what, dbx, infos, debug)
+    enc = jdba.jcommon.SingletonIO().default_encoding
+    if what == "a":
+        with open("sagas.tsv", "w", encoding=enc) as fdout:
+            dump_out(fdout, res)
     return res
 
 
@@ -44,6 +59,7 @@ def my_script(what, dbx, infos, debug=0):
     if debug > 0:
         print(jix.byname, end="\n" + "=" * 20 + "\n\n")
     seq = jix.get_ptr("CampSov57")
+    res = []
     for item in seq:
         an_id = item["Id"]
         if an_id <= 0:
@@ -53,7 +69,21 @@ def my_script(what, dbx, infos, debug=0):
         watch = "https://www.youtube.com/watch?v=" + this["Watch"]
         this["Watch"] = watch
         print(item)
-    return []
+        res.append(
+            (
+                str(item["Id"]),
+                item["Watch"],
+                item["Name"],
+                item["UDesc"],
+            )
+        )
+    return res
+
+
+def dump_out(fdout, seq):
+    for tup in seq:
+        there = '\t'.join(tup)
+        fdout.write(there + "\n")
 
 
 def get_db(debug=0) -> tuple:
@@ -63,7 +93,7 @@ def get_db(debug=0) -> tuple:
         "main-dlist": my_dlist,
     }
     """
-    dbx = jdba.database.Database("../vidlib")
+    dbx = jdba.database.Database("../vidlib", encoding=IO_ENCODING)
     is_ok = dbx.valid_schema()
     dbx.index_all()
     my_dlist = dbx.table("sagas").dlist
